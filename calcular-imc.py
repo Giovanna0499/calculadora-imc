8d4c58c00a03be9c4d0c4991eb214e495bb96ab9
Giovanna Oliveira <gobiribili@gmail.com>
2025-09-01 19:50:44 -0300

    Incluido Nome

--- calcular-imc.py
+++ calcular-imc.py
@@ -1,9 +1,10 @@
+nome = input("Digite seu nome: ")
 peso = float(input("Digite seu peso (em kg): "))
 altura = float(input("Digite sua altura (em metros): "))
 
 imc = peso / (altura ** 2)
 
-print("Seu IMC é: {:.2f}".format(imc))
+print("Olá, {}!".format(nome), "Seu IMC é: {:.2f}".format(imc))
 
 if imc < 18.5:
     print("Abaixo do peso")
